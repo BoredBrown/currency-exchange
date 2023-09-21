62c4a087e42eb4f6e4a836bd18113898e5b9b7d5
BarnabasK <barnabaspeter.kalasz@gmail.com>
2023-09-21 19:00:23 +0200

    added main three endpoints

--- frontend/currency_exchange/src/api/currency-exchange.api.ts
+++ frontend/currency_exchange/src/api/currency-exchange.api.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
+
+
 export const getSymbols = () => {
     axios.get('http://localhost:3000/symbols', {
-        
-        // Add parameters here
-      })
+              })
       .then((response) => {
         console.log(response.data);
-          // Handle data
       })
       .catch((error) => {
         console.log(error);
